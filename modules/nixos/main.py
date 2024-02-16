3be7e0998f9753afec64d4860191f524361033ea
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2024-02-16 08:46:43 +0100

    Use proxy when installing, if set

--- modules/nixos/main.py
+++ modules/nixos/main.py
@@ -321,6 +321,22 @@ cfgtail = """  # Some programs need SUID wrappers, can be configured further or 
 """
 
 
+def generateProxyStrings():
+    proxyEnv = []
+    if env_is_set('http_proxy'):
+        proxyEnv.append('http_proxy={}'.format(os.environ.get('http_proxy')))
+    if env_is_set('https_proxy'):
+        proxyEnv.append('https_proxy={}'.format(os.environ.get('https_proxy')))
+    if env_is_set('HTTP_PROXY'):
+        proxyEnv.append('HTTP_PROXY={}'.format(os.environ.get('HTTP_PROXY')))
+    if env_is_set('HTTPS_PROXY'):
+        proxyEnv.append('HTTPS_PROXY={}'.format(os.environ.get('HTTPS_PROXY')))
+
+    if proxyEnv != "":
+        proxyEnv.insert(0, "env")
+
+    return proxyEnv
+
 def pretty_name():
     return _("Installing NixOS.")
 
@@ -650,10 +666,28 @@ def run():
     status = _("Installing NixOS")
     libcalamares.job.setprogress(0.3)
 
+    # build nixos-install command
+    nixosInstallCmd = [ "pkexec" ]
+    nixosInstallCmd.extend(generateProxyStrings)
+    nixosInstallCmd.extend(
+        [
+            "nixos-install",
+            "--no-root-passwd",
+            "-I",
+            'NIXPATH="nixpkgs=nixos-config=/etc/nixos/configuration.nix:nixpkgs={}/etc/nixos/nixpkgs"'.format(root_mount_point),
+            "--root",
+            root_mount_point
+        ]
+    )
+
     # Install customizations
     try:
         output = ""
-        proc = subprocess.Popen(["pkexec", "nixos-install", "--no-root-passwd", "--root", root_mount_point], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(
+            nixosInstallCmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
         while True:
             line = proc.stdout.readline().decode("utf-8")
             output += line
