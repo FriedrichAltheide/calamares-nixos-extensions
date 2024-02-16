efec5ff2880375f45a6b467ab1bb4f1bd1818aab
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2024-02-16 08:47:39 +0100

    Allow building installers with custom configs
    - Move previous config snippets into snippet files, used as default snippets (defaultConfigSnippets)
    - If a custom snippet is found in customConfigSnippets, it is used instead

--- modules/nixos/main.py
+++ modules/nixos/main.py
@@ -23,303 +23,28 @@ _ = gettext.translation("calamares-python",
 # The following strings contain pieces of a nix-configuration file.
 # They are adapted from the default config generated from the nixos-generate-config command.
 
-cfghead = """# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-"""
-cfgbootefi = """  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-"""
-
-cfgbootbios = """  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "@@bootdev@@";
-  boot.loader.grub.useOSProber = true;
-
-"""
-
-cfgbootnone = """  # Disable bootloader.
-  boot.loader.grub.enable = false;
-
-"""
-
-cfgbootgrubcrypt = """  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-
-  boot.loader.grub.enableCryptodisk=true;
-
-"""
-
-cfgnetwork = """  networking.hostName = "@@hostname@@"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-"""
-
-cfgnetworkmanager = """  # Enable networking
-  networking.networkmanager.enable = true;
-
-"""
-
-cfgconnman = """  # Enable networking
-  services.connman.enable = true;
-
-"""
-
-cfgnmapplet = """  # Enable network manager applet
-  programs.nm-applet.enable = true;
-
-"""
-
-cfgtime = """  # Set your time zone.
-  time.timeZone = "@@timezone@@";
-
-"""
-
-cfglocale = """  # Select internationalisation properties.
-  i18n.defaultLocale = "@@LANG@@";
-
-"""
-
-cfglocaleextra = """  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "@@LC_ADDRESS@@";
-    LC_IDENTIFICATION = "@@LC_IDENTIFICATION@@";
-    LC_MEASUREMENT = "@@LC_MEASUREMENT@@";
-    LC_MONETARY = "@@LC_MONETARY@@";
-    LC_NAME = "@@LC_NAME@@";
-    LC_NUMERIC = "@@LC_NUMERIC@@";
-    LC_PAPER = "@@LC_PAPER@@";
-    LC_TELEPHONE = "@@LC_TELEPHONE@@";
-    LC_TIME = "@@LC_TIME@@";
-  };
-
-"""
-
-cfggnome = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-"""
-
-cfgplasma = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-"""
-
-cfgxfce = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the XFCE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
-
-"""
-
-cfgpantheon = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Pantheon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.pantheon.enable = true;
-
-"""
-
-cfgcinnamon = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Cinnamon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.cinnamon.enable = true;
-
-"""
-
-cfgmate = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the MATE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.mate.enable = true;
-
-"""
-
-cfgenlightenment = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Enlightenment Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.enlightenment.enable = true;
-
-  # Enable acpid
-  services.acpid.enable = true;
-
-"""
-
-cfglxqt = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the LXQT Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.lxqt.enable = true;
-
-"""
-
-cfglumina = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Lumina Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.lumina.enable = true;
-
-"""
-
-cfgbudgie = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Budgie Desktop environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.budgie.enable = true;
-
-"""
-
-cfgdeepin = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Deepin Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.deepin.enable = true;
-
-"""
-
-cfgkeymap = """  # Configure keymap in X11
-  services.xserver = {
-    layout = "@@kblayout@@";
-    xkbVariant = "@@kbvariant@@";
-  };
-
-"""
-cfgconsole = """  # Configure console keymap
-  console.keyMap = "@@vconsole@@";
-
-"""
-
-cfgmisc = """  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-"""
-cfgusers = """  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.@@username@@ = {
-    isNormalUser = true;
-    description = "@@fullname@@";
-    extraGroups = [ @@groups@@ ];
-    packages = with pkgs; [@@pkgs@@];
-  };
-
-"""
-
-cfgautologin = """  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "@@username@@";
-
-"""
-
-cfgautologingdm = """  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-"""
-
-cfgautologintty = """  # Enable automatic login for the user.
-  services.getty.autologinUser = "@@username@@";
-
-"""
-
-cfgunfree = """  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-"""
-
-cfgpkgs = """  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
-
-"""
-
-cfgtail = """  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "@@nixosversion@@"; # Did you read the comment?
-
-}
-"""
-
+def getDefaultConfig(snippetName):
+    # Check if a custom snippet exists for the config name
+    configPath = '/run/current-system/sw/lib/calamares/modules/nixos'
+
+    snippetConfigPath = '{}/defaultConfigSnippets/{}.snippet'.format(configPath, snippetName)
+    customConfigFilePath = '{}/customConfigSnippets/{}.snippet'.format(configPath, snippetName)
+
+    if os.path.isfile(customConfigFilePath):
+        snippetConfigPath = customConfigFilePath
+
+    # load config
+    with open(snippetConfigPath, 'r') as configReader:
+        snippet = configReader.read()
+        snippet += '\n'
+        return snippet
+
+    libcalamares.utils.error('Failed to load config snippet {}'.format(snippetName))
+    return ""
+
+def env_is_set(name):
+    envValue = os.environ.get(name)
+    return not (envValue is None or envValue == "")
 
 def generateProxyStrings():
     proxyEnv = []
@@ -368,7 +93,7 @@ def run():
     libcalamares.job.setprogress(0.1)
 
     # Create initial config file
-    cfg = cfghead
+    cfg = getDefaultConfig('head')
     gs = libcalamares.globalstorage
     variables = dict()
 
@@ -383,12 +108,12 @@ def run():
 
     # Check bootloader
     if (fw_type == "efi"):
-        cfg += cfgbootefi
+        cfg += getDefaultConfig('bootefi')
     elif (bootdev != "nodev"):
-        cfg += cfgbootbios
+        cfg += getDefaultConfig('bootbios')
         catenate(variables, "bootdev", bootdev)
     else:
-        cfg += cfgbootnone
+        cfg += getDefaultConfig('bootnone')
 
     # Setup encrypted swap devices. nixos-generate-config doesn't seem to notice them.
     for part in gs.value("partitions"):
@@ -399,7 +124,7 @@ def run():
     # Check partitions
     for part in gs.value("partitions"):
         if part["claimed"] == True and part["fsName"] == "luks" and fw_type != "efi":
-            cfg += cfgbootgrubcrypt
+            cfg += getDefaultConfig('bootgrubcrypt')
             status = _("Setting up LUKS")
             libcalamares.job.setprogress(0.15)
             try:
@@ -431,14 +156,14 @@ def run():
     status = _("Configuring NixOS")
     libcalamares.job.setprogress(0.18)
 
-    cfg += cfgnetwork
+    cfg += getDefaultConfig('network')
     if gs.value("packagechooser_packagechooser") == "enlightenment":
-        cfg += cfgconnman
+        cfg += getDefaultConfig('connman')
     else:
-        cfg += cfgnetworkmanager
+        cfg += getDefaultConfig('networkmanager')
 
     if (gs.value("packagechooser_packagechooser") == "mate") | (gs.value("packagechooser_packagechooser") == "lxqt") | (gs.value("packagechooser_packagechooser") == "lumina"):
-        cfg += cfgnmapplet
+        cfg += getDefaultConfig('nmapplet')
 
     if (gs.value("hostname") is None):
         catenate(variables, "hostname", "nixos")
@@ -446,46 +171,46 @@ def run():
         catenate(variables, "hostname", gs.value("hostname"))
 
     if (gs.value("locationRegion") is not None and gs.value("locationZone") is not None):
-        cfg += cfgtime
+        cfg += getDefaultConfig('time')
         catenate(variables, "timezone", gs.value(
             "locationRegion"), "/", gs.value("locationZone"))
 
     if (gs.value("localeConf") is not None):
         localeconf = gs.value("localeConf")
         locale = localeconf.pop("LANG").split("/")[0]
-        cfg += cfglocale
+        cfg += getDefaultConfig('locale')
         catenate(variables, "LANG", locale)
         if (len(set(localeconf.values())) != 1 or list(set(localeconf.values()))[0] != locale):
-            cfg += cfglocaleextra
+            cfg += getDefaultConfig('localeextra')
             for conf in localeconf:
                 catenate(variables, conf, localeconf.get(conf).split("/")[0])
 
     # Choose desktop environment
     if gs.value("packagechooser_packagechooser") == "gnome":
-        cfg += cfggnome
+        cfg += getDefaultConfig('desktopEnv/gnome')
     elif gs.value("packagechooser_packagechooser") == "plasma":
-        cfg += cfgplasma
+        cfg += getDefaultConfig('desktopEnv/plasma')
     elif gs.value("packagechooser_packagechooser") == "xfce":
-        cfg += cfgxfce
+        cfg += getDefaultConfig('desktopEnv/xfce')
     elif gs.value("packagechooser_packagechooser") == "pantheon":
-        cfg += cfgpantheon
+        cfg += getDefaultConfig('desktopEnv/pantheon')
     elif gs.value("packagechooser_packagechooser") == "cinnamon":
-        cfg += cfgcinnamon
+        cfg += getDefaultConfig('desktopEnv/cinnamon')
     elif gs.value("packagechooser_packagechooser") == "mate":
-        cfg += cfgmate
+        cfg += getDefaultConfig('desktopEnv/mate')
     elif gs.value("packagechooser_packagechooser") == "enlightenment":
-        cfg += cfgenlightenment
+        cfg += getDefaultConfig('desktopEnv/enlightenment')
     elif gs.value("packagechooser_packagechooser") == "lxqt":
-        cfg += cfglxqt
+        cfg += getDefaultConfig('desktopEnv/lxqt')
     elif gs.value("packagechooser_packagechooser") == "lumina":
-        cfg += cfglumina
+        cfg += getDefaultConfig('desktopEnv/lumina')
     elif gs.value("packagechooser_packagechooser") == "budgie":
-        cfg += cfgbudgie
+        cfg += getDefaultConfig('desktopEnv/budgie')
     elif gs.value("packagechooser_packagechooser") == "deepin":
-        cfg += cfgdeepin
+        cfg += getDefaultConfig('desktopEnv/deepin')
 
     if (gs.value("keyboardLayout") is not None and gs.value("keyboardVariant") is not None):
-        cfg += cfgkeymap
+        cfg += getDefaultConfig('keymap')
         catenate(variables, "kblayout", gs.value("keyboardLayout"))
         catenate(variables, "kbvariant", gs.value("keyboardVariant"))
 
@@ -493,7 +218,7 @@ def run():
             try:
                 subprocess.check_output(["pkexec", "loadkeys", gs.value(
                     "keyboardVConsoleKeymap").strip()], stderr=subprocess.STDOUT)
-                cfg += cfgconsole
+                cfg += getDefaultConfig('console')
                 catenate(variables, "vconsole", gs.value(
                     "keyboardVConsoleKeymap").strip())
             except subprocess.CalledProcessError as e:
@@ -532,7 +257,7 @@ def run():
                 try:
                     subprocess.check_output(
                         ["pkexec", "loadkeys", vconsole], stderr=subprocess.STDOUT)
-                    cfg += cfgconsole
+                    cfg += getDefaultConfig('console')
                     catenate(variables, "vconsole", vconsole)
                 except subprocess.CalledProcessError as e:
                     libcalamares.utils.error("loadkeys: {}".format(e.output))
@@ -542,32 +267,32 @@ def run():
                         gs.value("keyboardVConsoleKeymap")))
 
     if gs.value("packagechooser_packagechooser") is not None and gs.value("packagechooser_packagechooser") != "":
-        cfg += cfgmisc
+        cfg += getDefaultConfig('misc')
 
     if (gs.value("username") is not None):
         fullname = gs.value("fullname")
         groups = ["networkmanager", "wheel"]
 
-        cfg += cfgusers
+        cfg += getDefaultConfig('users')
         catenate(variables, "username", gs.value("username"))
         catenate(variables, "fullname", fullname)
         catenate(variables, "groups", (" ").join(
             ["\"" + s + "\"" for s in groups]))
         if (gs.value("autoLoginUser") is not None and gs.value("packagechooser_packagechooser") is not None and gs.value("packagechooser_packagechooser") != ""):
-            cfg += cfgautologin
+            cfg += getDefaultConfig('autologin')
             if (gs.value("packagechooser_packagechooser") == "gnome"):
-                cfg += cfgautologingdm
+                cfg += getDefaultConfig('autologingdm')
         elif (gs.value("autoLoginUser") is not None):
-            cfg += cfgautologintty
+            cfg += getDefaultConfig('autologintty')
 
     # Check if unfree packages are allowed
     free = True
     if gs.value("packagechooser_unfree") is not None:
         if gs.value("packagechooser_unfree") == "unfree":
             free = False
-            cfg += cfgunfree
+            cfg += getDefaultConfig('unfree')
 
-    cfg += cfgpkgs
+    cfg += getDefaultConfig('pkgs')
     # Use firefox as default as a graphical web browser, and add kate to plasma desktop
     if gs.value("packagechooser_packagechooser") == "plasma":
         catenate(variables, "pkgs", "\n      firefox\n      kate\n    #  thunderbird\n    ")
@@ -576,7 +301,10 @@ def run():
     else:
         catenate(variables, "pkgs", "")
 
-    cfg += cfgtail
+    # Add custom config
+    cfg += getDefaultConfig('extra')
+
+    cfg += getDefaultConfig('tail')
     version = ".".join(subprocess.getoutput(
         ["nixos-version"]).split(".")[:2])[:5]
     catenate(variables, "nixosversion", version)
